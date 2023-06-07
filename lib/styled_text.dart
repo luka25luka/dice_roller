13b3bcf30bf44038e363082fd9ee7ea45d8208b0
luka25luka <lukatchikadze@gmail.com>
2023-06-07 22:19:07 +0400

    my first app

--- lib/styled_text.dart
+++ lib/styled_text.dart
@@ -10,7 +10,7 @@ class StyledText extends StatelessWidget {
     return Text(
       text,
       style: const TextStyle(
-        color: Colors.white,
+        color: Color.fromARGB(255, 246, 246, 246),
         fontSize: 28,
       ),
     );
